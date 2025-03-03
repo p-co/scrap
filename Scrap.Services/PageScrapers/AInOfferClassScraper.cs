8edfbf3dd44a644f4d41241f674e3325c814262f
p-co <pierre.couderc@efrei.net>
2025-03-03 23:05:18 +0100

    feat: Another way to read a list in a page

---
+++ Scrap.Services/PageScrapers/AInOfferClassScraper.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using Scrap.Services.Abstractions;
+
+namespace Scrap.Services.PageScrapers
+{
+    internal class AInOfferClassScraper : IPageScraper
+    {
+        public IList<Tuple<string, string>> Scrap(IWebDriver webDriver)
+        {
+            var links = new List<Tuple<string, string>>();
+
+            var offerElements = webDriver.FindElements(By.ClassName("offerlist-item"));
+            foreach (var offerElement in offerElements)
+            {
+                var aElements = offerElement.FindElements(By.TagName("a"));
+                foreach (var aElement in aElements)
+                {
+                    string text = aElement.Text;
+                    string href = aElement.GetAttribute("href");
+                    links.Add(Tuple.Create(text, href));
+                }
+            }
+            return links;
+        }
+    }
+}
--- Scrap.Services/PageScrapers/PageScraperFactory.cs
+++ Scrap.Services/PageScrapers/PageScraperFactory.cs
@@ -11,6 +11,7 @@ namespace Scrap.Services.PageScrapers
                 "a in card div" => new AInCardDivScraper(),
                 "a in mat-panel-title" => new AInMatPanelTitleScraper(),
                 "a in table" => new AInTableScraper(),
+                "a in offer class" => new AInOfferClassScraper(),
                 "" => new AInUlScraper(),
                 _ => new AInUlScraper(),
             };
